ec186f06a447819333c2c5991d5695043706f282
jennifer sammy <jensam209@gmail.com>
2023-08-15 17:12:29 +0300

    first commit

---
+++ kenyanflag.py
@@ -0,0 +1,48 @@
+import matplotlib
+matplotlib.use('TkAgg') 
+import numpy as np
+import matplotlib.pyplot as py
+import matplotlib.patches as patches
+
+ 
+#  plotting the colors
+a = patches.Rectangle((0, 7), width=9, height=2, facecolor="#000000", edgecolor="grey")
+b = patches.Rectangle((0, 6.8), width=9, height=0.4, facecolor="#FFFFFF", edgecolor="grey")
+c = patches.Rectangle((0, 5), width=9, height=2, facecolor="#FF0000", edgecolor="grey")
+d = patches.Rectangle((0, 4.8), width=9, height=0.4, facecolor="#FFFFFF", edgecolor="grey")
+e = patches.Rectangle((0, 3), width=9, height=2, facecolor="#008000", edgecolor="grey")
+
+m,n = py.subplots()
+n.add_patch(a)
+n.add_patch(b)
+n.add_patch(c)
+n.add_patch(d)
+n.add_patch(e)
+
+# kenyan shield
+
+vertices = [
+    (4.5, 8),
+    (6, 5.5),
+    (4.5, 3),
+    (3, 5.5)
+]
+
+shield_polygon = patches.Polygon(vertices, closed=True, fill=False, edgecolor='black')
+
+# n.add_patch(spear)
+n.add_patch(shield_polygon)
+
+# Set the limits of the plot
+n.set_xlim(0, 9)
+n.set_ylim(0, 11) 
+
+# Remove the x and y ticks
+n.set_xticks([])
+n.set_yticks([])
+
+# Save the figure to a file
+py.savefig('kenyan_flag.png', bbox_inches='tight', dpi=300)
+
+# Display the image
+py.show()
